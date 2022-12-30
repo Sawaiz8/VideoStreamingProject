18b8656faf072f844f11f74c274c1959a84e82aa
Sawaiz <sawaiz@pop-os.localdomain>
2022-12-30 20:14:19 +0500

    Completed

--- client.py
+++ client.py
@@ -2,36 +2,36 @@ import cv2 as cv
 from socket import *
 import pickle
 import struct
+import imutils
 
-HEADER_SIZE = 64
-HEADER_FORMAT = "utf-8"
+HEADER = 8
+HEADER_FORMAT = "unicode_escape"
 SERVER_NAME = 'localhost'	# 'servername'
-SERVER_PORT = 12001
+SERVER_PORT = 12004
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((SERVER_NAME, SERVER_PORT))
-
+print("Starting video capture") 
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+        print("Cannot open camera")
 
 def start_client():
         connected = True
         while connected:
-                print("Starting video capture") 
-                cap = cv.VideoCapture(0)
-                if not cap.isOpened():
-                        print("Cannot open camera")
-
                 ret, frame = cap.read()
+           
                 if not ret:
                         print("Cannot recieve gram (stream end?) Exiting")
                         break
-
-                #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-                gray = frame
-
-                frame = pickle.dumps(frame)
-                msg_len = str(len(frame)).encode(HEADER_FORMAT)
-                msg_len += b' ' * (HEADER_SIZE -len(msg_len))
-                clientSocket.send(msg_len)
-                clientSocket.send(frame)
+                frame = imutils.resize(frame, width=320)
+                cv.imshow("Frame", frame)
+                
+                frame = pickle.dumps(frame, 0)
+                msg_len = len(frame)
+                msg_len_stream = struct.pack("Q", msg_len)
+		
+                clientSocket.sendall(msg_len_stream+frame)
 
                 if cv.waitKey(1) == ord("q"):
                         break           
